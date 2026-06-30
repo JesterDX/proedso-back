b98829b7e39bbbaed745aad07081ae8e7424cbd3
JesterDX <galvezascoy@gmail.com>
2026-06-30 10:48:28 -0500

    Add PUT and PATCH routes for tipos-curso

--- src/routes/tipos-curso.routes.js
+++ src/routes/tipos-curso.routes.js
@@ -4,6 +4,11 @@ const controller = require('../controllers/tipos-curso.controller');
 const router = express.Router();
 
 router.get('/', controller.listar);
+
 router.post('/', controller.crear);
+
+router.put('/:id', controller.actualizar);
+
+router.patch('/:id/estado', controller.cambiarEstado);
 
 module.exports = router;
