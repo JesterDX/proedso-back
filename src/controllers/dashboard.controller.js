c49d5c6f3c52b2991ba25fbefab36c744572ac50
JesterDX <galvezascoy@gmail.com>
2026-07-01 15:55:16 -0500

    Update dashboard.controller.js

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,18 +2,23 @@ const dashboardService = require('../services/dashboard.service');
 
 exports.getDashboard = async (req, res) => {
   try {
+
     const data = await dashboardService.obtenerDashboard();
 
-    res.json({
+    return res.status(200).json({
       success: true,
+      message: 'Dashboard obtenido correctamente',
       data
     });
-  } catch (error) {
-    console.error(error);
 
-    res.status(500).json({
+  } catch (error) {
+
+    console.error('Error Dashboard:', error);
+
+    return res.status(500).json({
       success: false,
       message: 'Error obteniendo dashboard'
     });
+
   }
 };
