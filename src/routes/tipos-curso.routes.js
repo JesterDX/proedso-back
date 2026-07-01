f0a4effbaeec7bc58191a7c11e3e12593725d79b
JesterDX <galvezascoy@gmail.com>
2026-07-01 14:32:35 -0500

    Add route to list active courses

--- src/routes/tipos-curso.routes.js
+++ src/routes/tipos-curso.routes.js
@@ -9,6 +9,8 @@ router.post('/', controller.crear);
 
 router.put('/:id', controller.actualizar);
 
+router.get('/activos',controller.listarActivos);
+
 router.patch('/:id/estado', controller.cambiarEstado);
 
 module.exports = router;
