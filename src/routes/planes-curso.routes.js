9cc93fde88940d1e079202e38d62c83b1066db72
JesterDX <galvezascoy@gmail.com>
2026-07-01 13:18:29 -0500

    Update planes-curso.routes.js

--- src/routes/planes-curso.routes.js
+++ src/routes/planes-curso.routes.js
@@ -4,6 +4,7 @@ const controller = require('../controllers/planes-curso.controller');
 const router = express.Router();
 
 router.get('/', controller.listar);
+router.get('/activos', controller.listarPlanesCursoActivos);
 router.post('/', controller.crear);
 
 module.exports = router;
