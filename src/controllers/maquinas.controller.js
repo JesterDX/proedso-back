dc0ab63bde143b13b030361a49728dd1efed55e8
JesterDX <galvezascoy@gmail.com>
2026-07-06 17:04:37 -0500

    Update maquinas.controller.js

--- src/controllers/maquinas.controller.js
+++ src/controllers/maquinas.controller.js
@@ -18,6 +18,67 @@ async function listar(req, res) {
   }
 }
 
+async function crear(req, res) {
+  try {
+    const data = await maquinasService.crearMaquina(req.body);
+
+    res.status(201).json({
+      ok: true,
+      message: 'Máquina creada correctamente.',
+      data
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      ok: false,
+      message: 'Error al crear máquina.'
+    });
+  }
+}
+
+async function actualizar(req, res) {
+  try {
+    const data = await maquinasService.actualizarMaquina(
+      req.params.id,
+      req.body
+    );
+
+    res.json({
+      ok: true,
+      data
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      ok: false,
+      message: 'Error al actualizar máquina.'
+    });
+  }
+}
+
+async function cambiarEstado(req, res) {
+  try {
+    const data = await maquinasService.cambiarEstadoMaquina(req.params.id);
+
+    res.json({
+      ok: true,
+      data
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      ok: false,
+      message: 'Error al cambiar estado.'
+    });
+  }
+}
+
 module.exports = {
-  listar
+  listar,
+  cambiarEstado,
+  actualizar,
+  crear
 };
