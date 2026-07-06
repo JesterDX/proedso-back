004368715507f7fbb9b2303f1ac0e7d892641b8b
JesterDX <galvezascoy@gmail.com>
2026-07-06 17:15:09 -0500

    Update maquinas.service.js

--- src/services/maquinas.service.js
+++ src/services/maquinas.service.js
@@ -20,6 +20,61 @@ async function listarMaquinas() {
   }
 }
 
+async function crearMaquina(data) {
+  const query = `
+    INSERT INTO maquinas
+    (
+      nombre,
+      orden_visual,
+      activo
+    )
+    VALUES ($1,$2,true)
+    RETURNING *;
+  `;
+
+  const result = await pool.query(query, [
+    data.nombre,
+    data.orden_visual
+  ]);
+
+  return result.rows[0];
+}
+
+async function actualizarMaquina(id, data) {
+  const query = `
+    UPDATE maquinas
+    SET
+      nombre = $1,
+      orden_visual = $2
+    WHERE id = $3
+    RETURNING *;
+  `;
+
+  const result = await pool.query(query, [
+    data.nombre,
+    data.orden_visual,
+    id
+  ]);
+
+  return result.rows[0];
+}
+
+async function cambiarEstadoMaquina(id) {
+  const query = `
+    UPDATE maquinas
+    SET activo = NOT activo
+    WHERE id = $1
+    RETURNING *;
+  `;
+
+  const result = await pool.query(query, [id]);
+
+  return result.rows[0];
+}
+
 module.exports = {
-  listarMaquinas
+  listarMaquinas,
+  crearMaquina,
+  actualizarMaquina,
+  cambiarEstadoMaquina
 };
