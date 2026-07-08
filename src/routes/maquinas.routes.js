ff7fd7bf8f8485137c46a2384db913c8335bf07a
JesterDX <galvezascoy@gmail.com>
2026-07-08 10:06:16 -0500

    Update maquinas.routes.js

--- src/routes/maquinas.routes.js
+++ src/routes/maquinas.routes.js
@@ -4,8 +4,9 @@ const controller = require('../controllers/maquinas.controller');
 const router = express.Router();
 
 router.get('/', controller.listar);
+router.get('/todas', controller.listarTodas);
+
 router.post('/', controller.crear);
 router.put('/:id', controller.actualizar);
 router.patch('/:id/estado', controller.cambiarEstado);
-
 module.exports = router;
