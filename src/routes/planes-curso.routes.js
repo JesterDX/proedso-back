5452a72277ccb3bd42b5c006fd757b8819d921e6
JesterDX <galvezascoy@gmail.com>
2026-07-14 12:36:39 -0500

    Update planes-curso.routes.js

--- src/routes/planes-curso.routes.js
+++ src/routes/planes-curso.routes.js
@@ -2,15 +2,54 @@ const express = require('express');
 const controller = require('../controllers/planes-curso.controller');
 
 const router = express.Router();
-router.get('/', controller.listar);
-router.get('/activos', controller.listarPlanesCursoActivos);
-router.get('/:id', controller.obtenerPorId);
-router.get('/:id/maquinas', controller.maquinas);
-router.post('/', controller.crear);
-router.put('/:id', controller.actualizar);
-router.patch('/:id/estado', controller.cambiarEstado);
-router.put(
- '/planes-curso/:id/configuracion',
- controller.guardarConfiguracion
+
+
+router.get(
+  '/',
+  controller.listar
 );
+
+
+router.get(
+  '/activos',
+  controller.listarPlanesCursoActivos
+);
+
+
+router.get(
+  '/:id/maquinas',
+  controller.maquinas
+);
+
+
+router.get(
+  '/:id',
+  controller.obtenerPorId
+);
+
+
+router.post(
+  '/',
+  controller.crear
+);
+
+
+router.put(
+  '/:id/configuracion',
+  controller.guardarConfiguracion
+);
+
+
+router.put(
+  '/:id',
+  controller.actualizar
+);
+
+
+router.patch(
+  '/:id/estado',
+  controller.cambiarEstado
+);
+
+
 module.exports = router;
