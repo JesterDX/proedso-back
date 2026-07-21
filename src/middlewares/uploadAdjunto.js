bfd5e3429deaa84b04e71b3875243466bf8c69bf
JesterDX <galvezascoy@gmail.com>
2026-07-21 14:09:47 -0500

    Update uploadAdjunto.js

--- src/middlewares/uploadAdjunto.js
+++ src/middlewares/uploadAdjunto.js
@@ -1,9 +1,10 @@
 const multer = require('multer');
 const { storageDocumentos } = require('../config/cloudinary');
 
-const uploadPago = multer({
-  storage: storageDocumentos, // 👈 Ahora usa Cloudinary
-  limits: { fileSize: 10 * 1024 * 1024 }
+// Middleware para procesar FormData y subir archivos a Cloudinary
+const uploadEvidencias = multer({
+  storage: storageDocumentos,
+  limits: { fileSize: 10 * 1024 * 1024 } // Límite de 10 MB
 });
 
-module.exports = uploadPago;
+module.exports = uploadEvidencias;
