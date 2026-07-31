cdce0526f75671fb2ce9814caa99f9e1cc79f6a3
JesterDX <galvezascoy@gmail.com>
2026-07-31 16:49:26 -0500

    Create homologaciones.routes.js

---
+++ src/routes/homologaciones.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const controller =
+require('../controllers/homologaciones.controller');
+
+router.get(
+
+    '/',
+
+    controller.listar
+
+);
+
+module.exports = router;
