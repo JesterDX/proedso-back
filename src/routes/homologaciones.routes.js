fe2097af89729944e5891be1bfd5d754dc252915
JesterDX <galvezascoy@gmail.com>
2026-08-04 17:31:18 -0500

    Update homologaciones.routes.js

--- src/routes/homologaciones.routes.js
+++ src/routes/homologaciones.routes.js
@@ -5,11 +5,58 @@ const router = express.Router();
 const controller =
 require('../controllers/homologaciones.controller');
 
+// ==========================================
+// LISTAR HOMOLOGACIONES
+// ==========================================
 router.get(
 
     '/',
 
     controller.listar
+
+);
+
+// ==========================================
+// OBTENER POR ID
+// ==========================================
+router.get(
+
+    '/:id',
+
+    controller.obtener
+
+);
+
+// ==========================================
+// CREAR
+// ==========================================
+router.post(
+
+    '/',
+
+    controller.crear
+
+);
+
+// ==========================================
+// ACTUALIZAR
+// ==========================================
+router.put(
+
+    '/:id',
+
+    controller.actualizar
+
+);
+
+// ==========================================
+// ELIMINAR
+// ==========================================
+router.delete(
+
+    '/:id',
+
+    controller.eliminar
 
 );
 
