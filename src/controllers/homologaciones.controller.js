a10bff9b21d79689444787302d499f620c9255b1
JesterDX <galvezascoy@gmail.com>
2026-08-04 17:26:35 -0500

    Update homologaciones.controller.js

--- src/controllers/homologaciones.controller.js
+++ src/controllers/homologaciones.controller.js
@@ -1,12 +1,10 @@
-const homologacionesService =
-require('../services/homologaciones.service');
+const service=require('../services/homologaciones.service');
 
 async function listar(req,res){
 
     try{
 
-        const data =
-        await homologacionesService.listarHomologaciones();
+        const data=await service.listarHomologaciones();
 
         res.json({
 
@@ -20,7 +18,38 @@ async function listar(req,res){
 
     catch(error){
 
-        console.log(error);
+        res.status(500).json({
+
+            ok:false,
+
+            message:error.message
+
+        });
+
+    }
+
+}
+async function obtener(req,res){
+
+    try{
+
+        const data=await service.obtenerHomologacion(
+
+            req.params.id
+
+        );
+
+        res.json({
+
+            ok:true,
+
+            data
+
+        });
+
+    }
+
+    catch(error){
 
         res.status(500).json({
 
@@ -34,8 +63,118 @@ async function listar(req,res){
 
 }
 
+async function crear(req,res){
+
+    try{
+
+        const data=await service.crearHomologacion(
+
+            req.body
+
+        );
+
+        res.json({
+
+            ok:true,
+
+            data
+
+        });
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+
+            ok:false,
+
+            message:error.message
+
+        });
+
+    }
+
+}
+
+async function actualizar(req,res){
+
+    try{
+
+        const data=await service.actualizarEstado(
+
+            req.params.id,
+
+            req.body
+
+        );
+
+        res.json({
+
+            ok:true,
+
+            data
+
+        });
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+
+            ok:false,
+
+            message:error.message
+
+        });
+
+    }
+
+}
+async function eliminar(req,res){
+
+    try{
+
+        await service.eliminarHomologacion(
+
+            req.params.id
+
+        );
+
+        res.json({
+
+            ok:true,
+
+            message:"Registro eliminado."
+
+        });
+
+    }
+
+    catch(error){
+
+        res.status(500).json({
+
+            ok:false,
+
+            message:error.message
+
+        });
+
+    }
+
+}
 module.exports={
 
-    listar
+    listar,
+
+    obtener,
+
+    crear,
+
+    actualizar,
+
+    eliminar
 
 };
